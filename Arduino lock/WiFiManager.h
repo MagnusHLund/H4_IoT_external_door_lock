b4480edd82efb0e9a4cd105d01ae1bf9472e5c4e
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-12-09 13:04:29 +0100

    changed mac address separator from : to -

--- Arduino lock/WiFiManager.h
+++ Arduino lock/WiFiManager.h
@@ -59,7 +59,7 @@ class WiFiManager {
       byte mac[6];
       WiFi.macAddress(mac);
     
-      snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+      snprintf(macStr, sizeof(macStr), "%02X-%02X-%02X-%02X-%02X-%02X",
                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     
       return macStr; 
