8b63d6a8214b52b3382fc329298fff48150338a6
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2025-12-10 15:47:21 +0100

    Added new Arduino project, for writing to the EEPROM. Other Arduino projects only read from the EEPROM.

---
+++ Arduino EEPROM/EEPROM.ino
@@ -0,0 +1,35 @@
+#include <EEPROM.h>
+#include "Config.h"
+#include "Secrets.h"
+
+Config config;
+
+void setup() {
+  Serial.begin(9600);
+
+  config.magic = 0xAFEDAFCB;
+  strcpy(config.wifi_ssid, WIFI_SSID);
+  strcpy(config.wifi_password, WIFI_PASSWORD);
+  strcpy(config.wifi_static_ip, WIFI_STATIC_IP);
+  strcpy(config.wifi_subnet_mask, WIFI_SUBNET_MASK);
+  strcpy(config.wifi_gateway, WIFI_GATEWAY);
+  strcpy(config.wifi_dns_server, WIFI_DNS_SERVER);
+  strcpy(config.mqtt_hostname, MQTT_HOSTNAME);
+  config.mqtt_port = MQTT_PORT;
+  strcpy(config.mqtt_username, MQTT_USERNAME);
+  strcpy(config.mqtt_password, MQTT_PASSWORD);
+  strcpy(config.ca_certificate, CA_CERTIFICATE);
+    
+  config.motor_pin = MOTOR_PIN;
+  config.pair_button_pin = PAIR_BUTTON_PIN;
+
+  // Write once to EEPROM
+  EEPROM.begin();
+  EEPROM.put(0, config);
+
+  Serial.println("Provisioning complete. Config saved to EEPROM.");
+}
+
+void loop() {
+  // Not required
+}
--- Arduino lock/ConfigManager.h
+++ Arduino lock/ConfigManager.h
@@ -1,49 +1,14 @@
 #include "Config.h"
+#include <EEPROM.h>
 
 class ConfigManager {
   public:
-    static void ConfigureEepromValues() {
-      Config config;
-      config.magic = 0xAFEDAFCB;
-
-      strcpy(config.wifi_ssid, "MySSID");
-      strcpy(config.wifi_password, "MyPassword");
-      strcpy(config.wifi_static_ip, "192.168.1.50");
-      strcpy(config.wifi_subnet_mask, "255.255.255.0");
-      strcpy(config.wifi_gateway, "192.168.1.1");
-      strcpy(config.wifi_dns_server, "1.1.1.1");
-
-      strcpy(config.mqtt_hostname, "192.168.1.100");
-      config.mqtt_port = 8883;
-      strcpy(config.mqtt_username, "mqttuser");
-      strcpy(config.mqtt_password, "mqttpass");
-
-            strcpy(config.ca_certificate, 
-       R"EOF(-----BEGIN CERTIFICATE-----
-        -----END CERTIFICATE-----)EOF");
-
-      config.motor_pin = 2;
-      config.pair_button_pin = 3;
-
-      // Saves config to the EEPROM
-      saveConfig(config);
-    }
-
-  private:
     static void LoadConfig(Config& config) {
       EEPROM.begin();          // no size argument
       EEPROM.get(0, config);   // read struct from address 0
 
       if (config.magic != 0xAFEDAFCB) {
-        ConfigureEepromValues();
         EEPROM.get(0, config);
       }
-    }
-  
-  private:
-    static void SaveConfig(const Config& config) {
-      EEPROM.begin();          // no size argument
-      EEPROM.put(0, config);   // write struct at address 0
-      // no commit() needed on Uno R4 WiFi
     }
 };
